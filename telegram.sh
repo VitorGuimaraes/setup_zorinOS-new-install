16bb480665ba4e5256d15f4c1287b7b9ed6780ec
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-05-18 17:47:56 -0300

    telegram sh install

---
+++ telegram.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://telegram.org/dl/desktop/linux
+sudo tar -xf linux -C /opt 
+sudo rm linux
+
+printf "\n\n==================="
+printf "\nTelegram installed!\n"
+echo "==================="
