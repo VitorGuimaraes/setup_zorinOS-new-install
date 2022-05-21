0d2bdef77b9bedb39aecae3fe9083586b1284091
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-05-21 19:10:17 -0300

    correction in files location

--- src/brightness-and-temperature.sh
+++ src/brightness-and-temperature.sh
@@ -3,10 +3,10 @@
 printf "\nInstalling Brightness and Temperature Control...\n\n"
 sleep 1
 
-cp -v ../brightness-and-temperature/brightness-decrease.sh ~/.brightness-decrease.sh
-cp -v ../brightness-and-temperature/brightness-increase.sh ~/.brightness-increase.sh
-cp -v ../brightness-and-temperature/color-temp-decrease.sh ~/.color-temp-decrease.sh
-cp -v ../brightness-and-temperature/color-temp-increase.sh ~/.color-temp-increase.sh
+cp -v brightness-and-temperature/brightness-decrease.sh ~/.brightness-decrease.sh
+cp -v brightness-and-temperature/brightness-increase.sh ~/.brightness-increase.sh
+cp -v brightness-and-temperature/color-temp-decrease.sh ~/.color-temp-decrease.sh
+cp -v brightness-and-temperature/color-temp-increase.sh ~/.color-temp-increase.sh
 
 xfconf-query --create --channel xfce4-keyboard-shortcuts --property "/commands/custom/<Alt>Left" --type string --set ~/.brightness-decrease.sh
 xfconf-query --create --channel xfce4-keyboard-shortcuts --property "/commands/custom/<Alt>Right" --type string --set ~/.brightness-increase.sh
