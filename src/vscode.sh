4f956392e4065b2efe44d650e09d04d5475a7cfd
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-05-21 18:17:49 -0300

    change vs code installation mode

--- src/vscode.sh
+++ src/vscode.sh
@@ -4,10 +4,10 @@ printf "\nInstalling VS Code...\n\n"
 sleep 1
 
 sudo apt update -y
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install code -y
 
 clear
 printf "================="
