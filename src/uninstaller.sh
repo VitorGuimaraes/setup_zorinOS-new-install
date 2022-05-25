95e29134c0fd04b27ea6e1962b1e1490c1c4fe7d
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-05-25 05:29:52 -0300

    fix emacs uninstall command

--- src/uninstaller.sh
+++ src/uninstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt purge 'libreoffice*' emacs mousepad onboard info xfburn gnome-mines sgt-puzzles gnome-sudoku gimp pidgin thunderbird transmission-gtk parole gigolo
+sudo apt purge 'libreoffice*' 'emacs*' mousepad onboard info xfburn gnome-mines sgt-puzzles gnome-sudoku gimp pidgin thunderbird transmission-gtk parole gigolo
 sudo apt clean 
 sudo apt autoremove 
 sudo apt -f install
