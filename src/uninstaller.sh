3c373fc347443ebbdfbe2647d3cc44228899650e
Vitor Guimarães <vitorrguimaraess@gmail.com>
2022-06-10 09:11:01 -0300

    Update uninstaller.sh

--- src/uninstaller.sh
+++ src/uninstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt purge 'libreoffice*' 'emacs*' mousepad onboard info xfburn gnome-mines sgt-puzzles gnome-sudoku gimp pidgin thunderbird transmission-gtk parole gigolo
+sudo apt purge 'libreoffice*' 'emacs*' 'redshift*' 'remmina*' '*cheese*' '*mahjongg*' aisleriot quadrapassel gnome-sound-recorder pitivi mousepad onboard info xfburn gnome-mines sgt-puzzles gnome-sudoku gimp pidgin thunderbird transmission-gtk parole gigolo
 sudo apt clean 
 sudo apt autoremove 
 sudo apt -f install
