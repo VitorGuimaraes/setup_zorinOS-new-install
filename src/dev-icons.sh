5569e4519d79f9fd75cda67d7b1b086b19cd0ee8
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2022-08-03 08:43:33 -0300

    Best files copy strategy Fix elixir icon with a xml file Fix dockerfile xml pattern. Now the icon is recognized for any Dockerfile with "ocker" substring in filename

--- src/dev-icons.sh
+++ src/dev-icons.sh
@@ -6,40 +6,37 @@ sleep 1
 mkdir -p logs 
 printf "" > logs/logs_dev_icons.txt
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ sudo cp -v DevIcons/dockerfile.xml /usr/share/mime/packages\n\n%s" \
-"$(sudo cp -v DevIcons/dockerfile.xml /usr/share/mime/packages)" \
-| tee -a logs/logs_dev_icons.txt
+# Copy xml files
+sudo cp -v DevIcons/*.xml /usr/share/mime/packages
 
-dest="/usr/share/icons/Zorin/16x16/mimetypes    /usr/share/icons/Zorin/24x24/mimetypes 
-	  /usr/share/icons/Zorin/16x16@2x/mimetypes /usr/share/icons/Zorin/24x24@2x/mimetypes
-      /usr/share/icons/Zorin/32x32/mimetypes    /usr/share/icons/Zorin/48x48/mimetypes
-      /usr/share/icons/Zorin/32x32@2x/mimetypes /usr/share/icons/Zorin/48x48@2x/mimetypes
-      /usr/share/icons/Zorin/512x512/mimetypes  /usr/share/icons/Zorin/512x512@2x/mimetypes"
+# copy png and svg files
+count=0
+for directory in /usr/share/icons/*/*/; do 
+	if [[ "$directory" == *"x"* ]]; then
+		for file in DevIcons/*.png; do
+			sudo cp -v $file $directory
+			count=$((count+1))
+		done
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-x-script.png\n\n%s" \
-"$(echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-x-script.png)" \
-| tee -a logs/logs_dev_icons.txt
+	elif [[ "$directory" == *"scalable"* ]]; then
+		for file in DevIcons/*.svg; do
+			sudo cp -v $file $directory
+			count=$((count+1))
+		done
+	fi
+done
+echo $count
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-html.png\n\n%s" \
-"$(echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-html.png)" \
-| tee -a logs/logs_dev_icons.txt
+# update icon-cache
+for directory in /usr/share/icons/*/; do 
+    sudo gtk-update-icon-cache $directory
+done
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-x-matlab.png\n\n%s" \
-"$(echo $dest | xargs -n 1 sudo cp -v DevIcons/icons/text-x-matlab.png)" \
-| tee -a logs/logs_dev_icons.txt
+# update mime-database
+sudo update-mime-database /usr/share/mime
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ sudo cp -v DevIcons/icons/*.svg /usr/share/icons/hicolor/scalable/mimetypes\n\n%s" \
-"$(sudo cp -v DevIcons/icons/*.svg /usr/share/icons/hicolor/scalable/mimetypes)" \
-| tee -a logs/logs_dev_icons.txt
+# clear
 
-printf "\n\n************************ $(date +"%D %T") ************************ 
-→ sudo gtk-update-icon-cache /usr/share/icons/hicolor/ -f\n\n%s" \
-"$(sudo gtk-update-icon-cache /usr/share/icons/hicolor/ -f)" \
-| tee -a logs/logs_dev_icons.txt
-
-clear
+printf "************************ $(date +"%D %T") ************************"
+printf "\nDev Icons installed!"
+printf "\n*******************************************************************\n"
