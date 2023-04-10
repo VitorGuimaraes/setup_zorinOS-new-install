3d6dd046f3ad3c306fd74472f83e3d65c3a0317d
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2023-04-10 08:30:51 -0300

    add git push alias

--- src/git.sh
+++ src/git.sh
@@ -11,6 +11,7 @@ git config --global alias.br branch
 git config --global alias.sw switch
 git config --global alias.st status
 git config --global alias.cm commit
+git config --global alias.ps push
 git config --global alias.pu pull
 
 git_check=$(whereis git)
