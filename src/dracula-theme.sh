1b06b8ba096706e5cf1f0ab0f6dfdbd92bf25627
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2023-05-21 12:13:11 -0300

    desabilitate Dracula Theme (conflict with ubuntu 22.04)

--- src/dracula-theme.sh
+++ src/dracula-theme.sh
@@ -3,36 +3,40 @@
 printf "\nInstalling Dracula Theme...\n"
 
 # grant that the files wasn't downloaded before
-sudo rm -rf -v gtk-master
-sudo rm -rf -v Dracula
+sudo rm -rf gtk-master
+sudo rm -rf Dracula
 
 wget https://github.com/dracula/gtk/archive/master.zip
 unzip master.zip 
-sudo rm -v master.zip 
-sudo mv -v gtk-master Dracula
-sudo mv -v Dracula /usr/share/themes
-cp -r /usr/share/themes/Dracula $HOME/.themes/Dracula
+sudo rm master.zip 
+sudo mv gtk-master Dracula
+sudo mv Dracula /usr/share/themes
+
+# Share themes with Flatpak
+sudo cp -r /usr/share/themes $HOME/.local/share/themes
+sudo cp -r /usr/share/icons $HOME/.local/share/icons
+sudo flatpak override --filesystem=$HOME/.local/share/themes
+sudo flatpak override --filesystem=$HOME/.local/share/icons
 
 # Apply Dracula theme to all system
-gsettings set org.gnome.shell.extensions.user-theme name 'Dracula'
-gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
-sudo flatpak override --filesystem=$HOME/.themes
+# gsettings set org.gnome.shell.extensions.user-theme name 'Dracula'
+# gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
 
 # install dconf
-sudo apt install dconf-editor -y
+# sudo apt install dconf-editor -y
 
 # load settings
-dconf load / < modern-terminal/user.conf
+# dconf load / < modern-terminal/user.conf
 
-# Apply dev-icons
-bash src/dev-icons.sh
+# # Apply dev-icons
+# bash src/dev-icons.sh
 
-dracula_theme_check=$(gsettings get org.gnome.desktop.interface gtk-theme)
-clear
+# dracula_theme_check=$(gsettings get org.gnome.desktop.interface gtk-theme)
+# clear
 
-if [[ "$dracula_theme_check" == *"Dracula"* ]]; then
-    printf "Dracula Theme installed!"
-else
-    printf "Dracula Theme not installed!"
-fi
-printf "\n*******************************************************************\n"
+# if [[ "$dracula_theme_check" == *"Dracula"* ]]; then
+#     printf "Dracula Theme installed!"
+# else
+#     printf "Dracula Theme not installed!"
+# fi
+# printf "\n*******************************************************************\n"
