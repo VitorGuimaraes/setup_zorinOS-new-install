8af028a7a8058d9fb2f9a4ab9086051296c5e994
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2023-05-21 12:14:34 -0300

    allow use Docker plugin in flatpack VsCode

--- src/vscode.sh
+++ src/vscode.sh
@@ -2,3 +2,7 @@
 
 flatpak install flathub com.visualstudio.code -y
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'com.visualstudio.code.desktop']"
+
+# allow dockerfile extension
+flatpak override --user --filesystem=/run/docker.sock com.visualstudio.code
+sed -i $'1a "docker.dockerPath": "/run/host/usr/bin/docker",' $HOME/.var/app/com.visualstudio.code/config/Code/User/settings.json
