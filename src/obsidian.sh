781d1f9691ffa2e291ff561208f35046902f7039
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2023-05-21 12:13:59 -0300

    auto get my Obsidian vault

--- src/obsidian.sh
+++ src/obsidian.sh
@@ -2,3 +2,10 @@
 
 flatpak install flathub md.obsidian.Obsidian -y
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'md.obsidian.Obsidian.desktop']"
+
+git_check=$(whereis git)
+if [[ "$git_check" != *"/usr/bin/git"* ]]; then
+    bash src/git.sh
+fi
+
+git clone https://github.com/vitorguimaraes/ObsidianVault.git $HOME/.ObsidianVault
