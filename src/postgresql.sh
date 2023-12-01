5c1f3ea06c42f8f3c114e26511e9b17df58047b9
Vitor Guimarães <vitorrguimaraess@gmail.com>
2023-12-01 04:32:48 -0300

    Update postgresql.sh

--- src/postgresql.sh
+++ src/postgresql.sh
@@ -4,8 +4,7 @@ printf "\nInstalling PostgreSQL...\n"
 
 sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" > /etc/apt/sources.list.d/PostgreSQL.list'
 wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/postgresql-archive-keyring.gpg >/dev/null 2>&1
-sudo apt update -y
-sudo apt install postgresql-16 postgresql-client-16 libpq-dev postgresql-server-dev-16 postgresql-contrib -y
+sudo apt update -y && sudo apt install postgresql-16 postgresql-client-16 libpq-dev postgresql-server-dev-16 postgresql-contrib -y
 
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '2345meia78'"
 
